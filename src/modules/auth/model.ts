187c390b199772d5177542b518d5380d96481ddd
Suleyman Laarabi <suleyman.laarabi@bleemeo.com>
2025-07-14 16:26:56 +0200

    feat: update docker-compose for Listmonk setup and enhance auth model validation

--- src/modules/auth/model.ts
+++ src/modules/auth/model.ts
@@ -2,25 +2,37 @@ import { t } from "elysia";
 import { UserModel } from "../user/model";
 
 export namespace AuthModel {
-  export const RegisterUserBody = t.Object({
-    email: t.String(),
-    firstName: t.String(),
-    lastName: t.String(),
-    username: t.String(),
-    password: t.String(),
-  });
-  export type RegisterUserBody = typeof RegisterUserBody.static;
+	export const RegisterUserBody = t.Object({
+		email: t.String({
+			format: "email",
+		}),
+		firstName: t.String({
+			minLength: 3,
+		}),
+		lastName: t.String({
+			minLength: 3,
+		}),
+		username: t.String({
+			minLength: 3,
+			maxLength: 20,
+			pattern: "^[a-zA-Z0-9_]+$",
+		}),
+		password: t.String({
+			minLength: 6,
+		}),
+	});
+	export type RegisterUserBody = typeof RegisterUserBody.static;
 
-  export const LoginUserBody = t.Object({
-    email: t.String(),
-    password: t.String(),
-  });
-  export type LoginUserBody = typeof LoginUserBody.static;
+	export const LoginUserBody = t.Object({
+		email: t.String(),
+		password: t.String(),
+	});
+	export type LoginUserBody = typeof LoginUserBody.static;
 
-  export const AuthenticatedUserSuccessResponse = t.Object({
-    user: UserModel.UserWithoutPassword,
-    token: t.String(),
-  });
-  export type AuthenticatedUserSuccessResponse =
-    typeof AuthenticatedUserSuccessResponse.static;
+	export const AuthenticatedUserSuccessResponse = t.Object({
+		user: UserModel.UserWithoutPassword,
+		token: t.String(),
+	});
+	export type AuthenticatedUserSuccessResponse =
+		typeof AuthenticatedUserSuccessResponse.static;
 }
